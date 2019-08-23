3d5ec396115468df1230ad3ddeebd9d96febebc5
Felipe Linares <Carlos.Linares.2019@upb.edu.co>
2019-08-23 12:11:49 -0500

    Ejercicio Resuelto

--- mayor.py
+++ mayor.py
@@ -1,6 +1,10 @@
 print("Bienvenido a el espacio donde sabras cuando un numero entero es mayor a cero")
-x=int(input("ingrese el numero "))
-if x>0:
-    print("es un numero mayor a cero")
+for i in range(1,11):
+    x=int(input("ingrese el numero "))
+    if x>0:
+        print("es un numero es postivo")
+
+    else:
+        print ("es un numero negativo")
 
 
